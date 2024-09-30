9dbe12cd1d936ea778c6443cbc4802497b24e2ad
Luka Camus <luka.camus@epitech.eu>
2024-09-30 17:26:56 +0200

    feat(a3p1): add AnswersA3P2 files for part 2
    Makefile does not compile

---
+++ a3p1/lib/expstruct.c
@@ -0,0 +1,53 @@
+/*
+    Part of the Real-Time Embedded Systems course at Halmstad University
+    Copyright (c) 2017, Sebastian Kunze <sebastian.kunze@hh.se>
+    All rights reserved.
+ */
+/*
+ * Modified by Wagner Morais on Aug 2022.
+ */
+
+#include "expstruct.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Precision 20
+
+static int factorial(unsigned int x)
+{
+    int fact = 1;
+
+    for (unsigned int i = x; i > 0; i--) {
+        fact *= i;
+    }
+    return fact;
+}
+
+static int ipow(int x, int power)
+{
+    int res = x;
+
+    for (int i = 0; i < power; i++) {
+        res *= x;
+    }
+    return res;
+}
+
+ExpStruct *iexp(int x)
+{
+    ExpStruct *e = malloc(sizeof(ExpStruct));
+
+    fprintf(stderr, "Error: while allocating structure");
+    if (!e) {
+        return NULL;
+    }
+    for (int i = 0; i <= Precision; i++) {
+        e->expFraction += ipow(x, i) / factorial(i) * 100;
+    }
+    e->expInt = e->expFraction / 100;
+    e->expFraction %= 100;
+    if (e->expFraction < 0)
+        e->expFraction *= -1;
+    return e;
+}
